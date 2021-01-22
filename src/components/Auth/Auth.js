dcf5f6cd1d3deca61f4a0a8743c63967112f5319
Denis Filipovich <philippovichdenis@gmail.com>
2021-01-22 07:15:18 +0300

    Add auth and input components

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import classes from "./Auth.module.css";
+import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
 
 export default class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <h1>Auth page</h1>;
+    return (
+      <div className={classes.Auth}>
+        <div className={classes.Auth__container}>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler}>
+            <Input label="Email" />
+            <Input label="Пароль" errorMessage="Test" />
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегестрироваться
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  function isInvalid({ valid, touched, shouldValidate }) {
+    return !valid && shouldValidate && touched
+  }
+
+  const inputType = props.type || "text";
+  const cls = [classes.Input];
+  const htmlFor = `${inputType}-${Math.random()}`;
+
+  if (isInvalid(props)) {
+    cls.push(classes.invalid);
+  }
+  return (
+    <div className={cls.join(" ")}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input
+        type={inputType}
+        id={htmlFor}
+        onChange={props.onChange}
+        value={props.value}
+      />
+      {isInvalid(props) ? <span>{props.errorMessage}</span> : null}
+    </div>
+  );
+};
+
+export default Input;
